6e4df3abc73303f21b0f94e0618fbd496b266ee9
tomlum <thlum@vassar.edu>
2016-10-05 14:02:56 -0400

    Re adjusts lab1app4atests wording

--- system/lab1app4tests.c
+++ system/lab1app4tests.c
@@ -6,25 +6,25 @@
  *  lab1app4tests  -  Process classification
  *------------------------------------------------------------------------
  */
-process lowPrioProc(void)
+process highPrioProc(void)
 {
 	struct procent *prptr;     /* table entry of current process */
 	prptr = &proctab[currpid];
 
 	sleepms(1);
 	while(prptr->prprio!=8){	
-		kprintf("\n): My Prio is %d, it needs to be lower!!!\n", prptr->prprio);
+		kprintf("\n): My Prio is only %d, it needs to be higher!!!\n", prptr->prprio);
 	}
-	kprintf("\n(: Low Prio Proc is Happy!\n");
+	kprintf("\n(: High Prio Proc is Happy!\n");
 }
 
-process highPrioProc(void)
+process lowPrioProc(void)
 {
 	struct procent *prptr;     /* table entry of current process */
 	prptr = &proctab[currpid];
 
 	while(prptr->prprio!=0){
-		kprintf("\n:( My Prio is only %d, it needs to be higher!!!\n", prptr->prprio);
+		kprintf("\n:( My Prio is only %d, it needs to be lower!!!\n", prptr->prprio);
 	}
-	kprintf("\n:) High Prio Proc is Happy!\n");
+	kprintf("\n:) Low Prio Proc is Happy!\n");
 }
